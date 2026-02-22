eab3c087808b88a90578e97785080da1076bd471
Artur <mr.arturio06@gmail.com>
2026-02-22 21:47:12 +0300

    remove unused code

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,3 @@
-declare module '*.module.css' {
-  interface IClassNames {
-    [className: string]: string;
-  }
-  const classNames: IClassNames;
-  export = classNames;
-}
-
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
